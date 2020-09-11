5306c7a55e8d218b08653d1ee38b00c141ba8111
ilhamnurilmi <71108123+ilhamnurilmi@users.noreply.github.com>
2020-09-11 11:40:01 +0700

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {
-
-	public static void main (String[] args) {
-		System.out.println("Nama:Ilham Nur Ilmi");
-		System.out.println("Nim :6706204115");
-		System.out.println("Kelas:D3IF4404");
-	}
-}

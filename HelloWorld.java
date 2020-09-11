178f223c3f2489473d498b4554d7723f352ec093
Miraz <miraznurihsan27@gmail.com>
2020-09-11 11:27:55 +0700

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+	public static void main (String[] args) {
+		System.out.println("Nama:Ilham Nur Ilmi");
+		System.out.println("Nim :6706204115");
+		System.out.println("Kelas:D3IF4404");
+	}
+}

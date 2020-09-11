7f9a8630cdeee36a8b0533d88d31c41f2538188d
Miraz <miraznurihsan27@gmail.com>
2020-09-11 11:37:26 +0700

    d3if.java

---
+++ d3if.java
@@ -0,0 +1,12 @@
+public class d3if {
+    /* program pertama
+    *programm kedua
+	
+    */
+    public static void main(String[] args){
+        //belajar program 
+        //kelas d3if
+		System.out.println("kelas 04");
+		System.out.println("angkatan 44");
+	}
+}
